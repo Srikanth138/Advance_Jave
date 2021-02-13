696d0c144e3689f8bf5fb06b2515134a384d10b3
srika <srika@LAPTOP-81P5HPDV.mshome.net>
2021-02-13 12:56:27 +0530

    update4

---
+++ PayrollSystem/src/com/sri/service/EmployerService.java
@@ -0,0 +1,38 @@
+package com.sri.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sri.bo.EmployerBO;
+import com.sri.dao.EmployerDAO;
+import com.sri.dao.IEmployer;
+import com.sri.dto.EmployerDTO;
+
+public class EmployerService {
+	IEmployer dao;
+	public EmployerService() {
+		dao=new EmployerDAO();
+	}
+	@SuppressWarnings("null")
+	public List<EmployerDTO> serviceEMP() {
+		List<EmployerBO> listbo=null;
+		//listbo=dao.delete();
+		List<EmployerDTO> listdto=new ArrayList<EmployerDTO>();
+		
+		
+		listbo.forEach(bo->{
+			EmployerDTO dto=new EmployerDTO();
+			dto.setId(bo.getId());
+			dto.setName(bo.getName());
+			dto.setPhone(bo.getPhone());
+			dto.setEmail(bo.getEmail());
+			dto.setCTC(bo.getCTC());
+			dto.setSalary(bo.getSalary());
+			dto.setGender(bo.getGender());
+			
+			listdto.add(dto);
+		});
+		
+		return  listdto;
+	}//method
+}//class
